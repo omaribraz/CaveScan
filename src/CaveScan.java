2b3744f33cb6142627d94dee99a17b57901d5472
omar ibraz <omaribraz@gmail.com>
2016-10-24 18:15:56 +0100

    Check in Mesh

--- src/CaveScan.java
+++ src/CaveScan.java
@@ -36,6 +36,7 @@ public class CaveScan extends PApplet {
     private ToxiclibsSupport gfx;
     TriangleMesh cave;
     private ArrayList<Vec3D> cavepts;
+    Vec3D meshcentre = new Vec3D();
 
     private WB_Render render;
     private HE_Mesh mesh;
@@ -79,6 +80,10 @@ public class CaveScan extends PApplet {
     ArrayList<Vec3D> pts = new ArrayList<>();
     ArrayList<Line3D> lines = new ArrayList<>();
 
+    ArrayList<Vec3D> pointsList = new ArrayList<>();
+
+    PeasyCam cam;
+
 
     public static void main(String[] args) {
         PApplet.main("CaveScan", args);
@@ -91,81 +96,59 @@ public class CaveScan extends PApplet {
 
     public void setup() {
 
-//        obj = loadShape("data/" + "drone.obj");
-//        obj.scale(3);
-//
-//        cone = loadShape("data/" + "cone.obj");
-//        cone.scale(5);
-//
-//        flock = new Flock(this);
-
-//        PeasyCam cam = new PeasyCam(this, 0, 0, 0, 220);
-
+        setupassets();
 
         meshsetup();
 
-        Vec3D a = cave.computeCentroid();
-        PeasyCam cam = new PeasyCam(this, a.x, a.y, 0, 2200);
+        readText();
 
 
-//        float DIM = 1500;
-//        meshoctree = new Octree(this, new Vec3D(-1, -1, -1).scaleSelf(a), DIM * 2);
-//        meshoctree.addAll(cavepts);
-//
-//        boidoctree = new Octree(this, new Vec3D(-1, -1, -1).scaleSelf(a), DIM * 2);
-//
-//        for (int i = 0; i < 25; i++) {
-//            flock.addBoid(new Boid(this, new Vec3D(random(0, 1200), random(0, 1200), random(190, 350)), new Vec3D(random(-TWO_PI, TWO_PI), random(-TWO_PI, TWO_PI), random(-TWO_PI, TWO_PI))));
-//        }
-//
+        cam = new PeasyCam(this, meshcentre.x, meshcentre.y, 0, 2200);
 
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
-                for (int k = 0; k < 20; k++) {
-                    pts.add(new Vec3D(i * 70, j * 70, k * 30));
+
+        float DIM = 1500;
+        meshoctree = new Octree(this, new Vec3D(-1, -1, -1).scaleSelf(meshcentre), DIM * 2);
+        meshoctree.addAll(cavepts);
+
+        boidoctree = new Octree(this, new Vec3D(-1, -1, -1).scaleSelf(meshcentre), DIM * 2);
+
+        for (int i = 0; i < 250; i++) {
+            flock.addBoid(new Boid(this, new Vec3D(random(0, 1200), random(0, 1200), random(190, 350)), new Vec3D(random(-TWO_PI, TWO_PI), random(-TWO_PI, TWO_PI), random(-TWO_PI, TWO_PI))));
+        }
+
+
+ //      meshpoints();
+
+
+        showOption[2] = true;
+        gs = new Graph();
+
+        Collections.reverse(pts);
+
+        for (int i = 0; i < pts.size(); i++) {
+            Vec3D f = pts.get(i);
+            if(i<2){
+                println(f);
+            }
+            gs.addNode(new GraphNode(i, f.x, f.y, f.z));
+            for (int j = 0; j < pts.size(); j++) {
+                Vec3D b = pts.get(j);
+                if (b != f) {
+                    if (b.distanceToSquared(f) < 75*75) {
+                        gs.addEdge(i, j, 0);
+//                        Line3D l1 = new Line3D(f, b);
+//                        lines.add(l1);
+                    }
                 }
             }
         }
 
 
-       for (int j = 0; j < 10; j++) {
-            for (int i = 0; i < pts.size(); i++) {
-                Vec3D v = pts.get(i);
-                Vec3D v1 = cave.getClosestVertexToPoint(v);
-                Vec3D v2 = v1.copy().subSelf(v.copy());
-                Vec3D v3 = Normal.get(v1);
-                float a1 = v3.angleBetween(v2,true);
-                float a2 = degrees(a1);
-                if(a2>90){
-                    pts.remove(v);
-                }
-            }
-      }
-
-
-//        showOption[2] = true;
-//        gs = new Graph();
-//        for (int i = 0; i < pts.size(); i++) {
-//            Vec3D f = pts.get(i);
-//            gs.addNode(new GraphNode(i, f.x, f.y, f.z));
-//            for (int j = 0; j < pts.size(); j++) {
-//                Vec3D b = pts.get(j);
-//                if (b != f) {
-//                    if (b.distanceTo(f) < 15) {
-//                        gs.addEdge(i, j, 0);
-//                        Line3D l1 = new Line3D(f, b);
-//                        lines.add(l1);
-//                    }
-//                }
-//            }
-//        }
-//
-//
-//        gNodes = gs.getNodeArray();
-//        gEdges = gs.getAllEdgeArray();
-//        start = 0;
-//        end = 568;
-//        gs.compact();
+        gNodes = gs.getNodeArray();
+        gEdges = gs.getAllEdgeArray();
+        start = 0;
+        end = 1;
+        gs.compact();
 
 
     }
@@ -175,61 +158,76 @@ public class CaveScan extends PApplet {
         background(0);
 
 
-//        if(frameCount>15) {
-        for (Vec3D a : pts) {
-            stroke(255);
-            strokeWeight(2);
-            point(a.x, a.y, a.z);
-        }
+//        for (Vec3D a : pts) {
+//            stroke(255);
+//            strokeWeight(2);
+//            point(a.x, a.y, a.z);
+//    }
 
-//        pathFinder = makePathFinder(3);
-//        usePathFinder(pathFinder);
-//
-//        drawEdges(exploredEdges, color(0, 0, 255), 1.8f);
-//
-//        drawRoute(rNodes, color(200, 0, 0), 5.0f);
-
-//        if (showOption[0]) {
-//            drawNodes();
-//        }
-
-//        for(Line3D l : lines ){
+//        for (Line3D l : lines) {
+//            strokeWeight(.1f);
 //            stroke(220);
 //            line(l.a.x, l.a.y, l.a.z, l.b.x, l.b.y, l.b.z);
 //        }
 
+        pathFinder = makePathFinder(3);
+        usePathFinder(pathFinder);
 
-//        for (Boid b : flock.boids) {
-//            boidoctree.addBoid(b);
-//        }
-//
-//        boidoctree.run();
-//
-//        flock.run();
-//
-//        if (frameCount < 10) {
-//            for (int i = 0; i < flock.boids.size(); i++) {
-//                Boid b = flock.boids.get(i);
-//                b.checkMesh();
-//            }
-//        }
+        drawEdges(exploredEdges, color(0, 0, 255), 1.8f);
+
+        drawRoute(rNodes, color(200, 0, 0), 5.0f);
+
+        if (showOption[0]) {
+            drawNodes();
+        }
+
+
+
+
+        for (Boid b : flock.boids) {
+            boidoctree.addBoid(b);
+        }
+
+        boidoctree.run();
+
+        if (frameCount < 10) {
+            for (int i = 0; i < flock.boids.size(); i++) {
+                Boid b = flock.boids.get(i);
+                b.checkMesh();
+            }
+        }
+
+ //       flock.run();
+
+
 
 
 //        boidoctree.draw();
 
 
-//        meshrun();
+        meshrun();
 
-        pushMatrix();
-        fill(40, 120);
-        noStroke();
-        lights();
-        gfx.mesh(cave, false, 0);
-        popMatrix();
+//        pushMatrix();
+//        fill(40, 120);
+//        noStroke();
+//        lights();
+//        gfx.mesh(cave, false, 0);
+//        popMatrix();
 
 //        videoExport.saveFrame();
         //       }
 
+    }
+
+
+    private void setupassets() {
+        obj = loadShape("data/" + "drone.obj");
+        obj.scale(3);
+
+        cone = loadShape("data/" + "cone.obj");
+        cone.scale(5);
+
+        flock = new Flock(this);
     }
 
 
@@ -254,6 +252,12 @@ public class CaveScan extends PApplet {
                 break;
         }
         return pf;
+    }
+
+    void usePathFinder(IGraphSearch pf){
+        pf.search(start, end, true);
+        rNodes = pf.getRoute();
+        exploredEdges = pf.getExaminedEdges();
     }
 
     void drawRoute(GraphNode[] r, int lineCol, float sWeight) {
@@ -301,21 +305,22 @@ public class CaveScan extends PApplet {
     }
 
     private void meshsetup() {
-        mesh = new HEC_FromOBJFile(sketchPath("data/" + "cave.obj")).create();
-        cave = (TriangleMesh) new STLReader().loadBinary(sketchPath("data/" + "cave.stl"), STLReader.TRIANGLEMESH);
+        mesh = new HEC_FromBinarySTLFile(sketchPath("data/" + "cave2.stl")).create();
+        cave = (TriangleMesh) new STLReader().loadBinary(sketchPath("data/" + "cave2.stl"), STLReader.TRIANGLEMESH);
+
+        meshcentre = cave.computeCentroid();
 
 
         gfx = new ToxiclibsSupport(this);
         render = new WB_Render(this);
 
 //        WB_KDTree vertexTree = mesh.getVertexTree();
+//        int novert1 = cave.getNumVertices();
+
         int novert = mesh.getNumberOfVertices();
 
         cave.flipVertexOrder();
-        mesh.flipFaces();
 
-
-//        int novert1 = cave.getNumVertices();
 
         cavepts = (new ArrayList<>(cave.getVertices()));
 
@@ -380,6 +385,48 @@ public class CaveScan extends PApplet {
 //        gfx.mesh(cave, false, 0);
 //        popMatrix();
 
+    }
+
+    private void meshpoints() {
+
+//        for (int i = 0; i < 20; i++) {
+//            for (int j = 0; j < 20; j++) {
+//                for (int k = 0; k < 20; k++) {
+//                    pts.add(new Vec3D(i * 70, j * 70, k * 30));
+//                }
+//            }
+//        }
+
+//        for (int j = 0; j < 10; j++) {
+//            for (int i = 0; i < pts.size(); i++) {
+//                Vec3D v = pts.get(i);
+//                Vec3D v1 = cave.getClosestVertexToPoint(v);
+////                Vec3D v2 = v1.copy().subSelf(v.copy());
+////                Vec3D v3 = Normal.get(v1);
+////                float a1 = v3.angleBetween(v2, true);
+////                float a2 = degrees(a1);
+////                if (a2 > 90) {
+////                    pts.remove(v);
+////                }
+//                float distpt = v1.distanceToSquared(v);
+//
+//                if (distpt < 55 * 55) {
+//                    pts.remove(this);
+//                }
+//            }
+//        }
+    }
+
+
+    private void readText() {
+        String[] attptList = loadStrings("data/" + "points.txt");
+        for (int i = attptList.length-1; i >= 0; i--) {
+            String point[] = (split(attptList[i], ','));
+            if (point.length==3) {
+                Vec3D Temp_PT = new Vec3D(Float.parseFloat(point[0]), Float.parseFloat(point[1]), Float.parseFloat(point[2]));
+                pts.add(Temp_PT);
+            }
+        }
     }
 
 }
